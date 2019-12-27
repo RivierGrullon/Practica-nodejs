83f18cfec307f187ec2946e083a69111e94042eb
RivierGrullon <rgrullon@ipisdosaj.edu.do>
2019-12-27 15:17:48 -0400

    some changes in index.js

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const express = require("express")
 
 const server = express();
 
-server.get("/", (req,res)=>{
-    res.send(<h1>Hola Mundo</h1>)
-    res.end()
+server.get('/', (req,res)=>{
+    res.send('<h1>Hola Mundo con Express y Node</h1>');
+    res.end();
 })
 
 server.listen(3000,()=>{
