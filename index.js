0c14974fdab648ed173741eb059722e24675b1b8
RivierGrullon <rgrullon@ipisdosaj.edu.do>
2019-12-27 15:04:05 -0400

    add new dependecies 'express'

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const http = require('http')
-const colors = require('colors')
-const HandleServer = function(req,res){
+// const http = require('http')
+// const colors = require('colors')
+// const HandleServer = function(req,res){
 
-    res.writeHead(200);
-    res.write("<h1>Hola Mundo con Node Js</h1>");
-    res.end();
+//     res.writeHead(200);
+//     res.write("<h1>Hola Mundo con Node Js</h1>");
+//     res.end();
 
-}
+// }
 
-const server = http.createServer(HandleServer)
+// const server = http.createServer(HandleServer)
 
-server.listen(3000,function(){
-    console.log("Server on port 3000".green)
-})
+// server.listen(3000,function(){
+//     console.log("Server on port 3000".green)
+// })
+const express = require("express")
+
+const server = express();
